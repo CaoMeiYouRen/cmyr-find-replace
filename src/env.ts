e4cc91bb584ce53b1823da131132775cf0fd60d5
CaoMeiYouRen <996881204@qq.com>
2022-05-24 01:16:16 +0800

    fix: 修复 版本号错误

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { version } = require('../package.json')
 export const __PROD__ = process.env.NODE_ENV === 'production'
 export const __DEV__ = process.env.NODE_ENV === 'development'
-export const __VERSION__ = process.env.VERSION
+export const __VERSION__ = version || process.env.VERSION
